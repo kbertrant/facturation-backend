38b25f128dccb6d026505d6368a9ff0335a44bf6
Bertrand KOUAM <bertrand.kouam.ext@socomar-cameroun.com>
2023-08-29 14:58:25 +0100

    first commit

---
+++ database/migrations/2023_07_04_090722_create_livraisons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLivraisonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livraisons', function (Blueprint $table) {
+            $table->id();
+            $table->string('lf_num')->unique();
+            $table->date('lf_date')->nullable();
+            $table->integer('lf_qte')->nullable();
+            $table->string('lf_stat')->default('A');
+            $table->unsignedBigInteger('id_four');
+            $table->unsignedBigInteger('id_prod');
+            $table->timestamps();
+            $table->foreign('id_four')->references('id')->on('fournisseurs');
+            $table->foreign('id_prod')->references('id')->on('produits');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livraisons');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\TresorerieController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+// routes/api.php
+Route::middleware('auth:api')->group( function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('tresorerie/list',[TresorerieController::class, 'index']);
+});
+
